c56f113a5032a190d2e2a9646e3abb4a0c806ba9
Sprowk <alexander8.matuska@gmail.com>
2018-12-06 12:08:56 +0100

    added the shortest path function

--- graf.py
+++ graf.py
@@ -89,4 +89,21 @@ class Graf:
         file.close()
 
     def najkratsia_cesta(self,od,kam):
-        pass
+        for i in range(len(self.mapa)):
+            self.mapa[i].nastav_vzdialenost(0)
+        stack = [self.mapa[od]]
+        dlzka = 0
+        while stack != []:
+            pocet = len(stack)
+            for i in range(len(stack)):
+                if stack[i] == self.mapa[kam]:
+                    return dlzka
+                susedia = stack[i].zisti_susedov()
+                for i2 in range(len(susedia)):
+                    if susedia[i2].zisti_vzdialenost() == 0:
+                        susedia[i2].nastav_vzdialenost(dlzka)
+                        stack.append(susedia[i2])
+            for i in range(pocet):
+                stack.pop(0)
+            dlzka += 1
+        return -1
