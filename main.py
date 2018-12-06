212e79ebfde2744c60b91a71785ee97f71e27b09
Sprowk <alexander8.matuska@gmail.com>
2018-12-06 11:03:08 +0100

    renamed variable

--- main.py
+++ main.py
@@ -5,24 +5,24 @@ c = Canvas(width=800, height=400)
 c.pack()
 c.focus_set()
 
-Igor = Graf(c,15)
-velkost = Igor.zisti_priemer()
+Mygraf = Graf(c,15)
+velkost = Mygraf.zisti_priemer()
 
 prvy = -1
 def pohyb(event):
     global prvy
     clicked = False
-    for i in range(Igor.zisti_pocet_vrcholov()):
-        pos = Igor.zisti_pos_vrchola(i)
+    for i in range(Mygraf.zisti_pocet_vrcholov()):
+        pos = Mygraf.zisti_pos_vrchola(i)
         if abs(pos[0] - event.x) < velkost and abs(pos[1] - event.y) < velkost:
             clicked = True
             if prvy == -1:
                 prvy = i
             elif prvy != i:
-                Igor.novy_sused(prvy, i)
+                Mygraf.novy_sused(prvy, i)
                 prvy = -1
     if not clicked:
-        Igor.pridaj(event.x, event.y, e.get())
+        Mygraf.pridaj(event.x, event.y, e.get())
         prvy = -1
 
 c.bind('<Button-1>', pohyb)
